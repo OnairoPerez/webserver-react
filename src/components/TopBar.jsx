ceabb417e13219c3913a1ec9c612026e9eba336d
Onairo Pérez <onairoperez48@gmail.com>
2024-07-02 09:18:40 -0500

    [Update] Evento onClick en componente TopBar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -7,6 +7,11 @@ import lupa from '../assets/icons/lupa.webp';
 import carrito from '../assets/icons/carrito.webp';
 import user from '../assets/icons/user.webp';
 
+//Redirigir a otra página de la web
+function handleClick(href) {
+  window.location.href = href
+}
+
 export default function TopBar() {
   return (
     <nav id="TopBar">
@@ -14,17 +19,17 @@ export default function TopBar() {
             <link rel="stylesheet" href="/css/topBar.css" />
         </Helmet>
         <div id="right" class="TopBarElements">
-            <img loading='lazy' id="logo" src={logo} alt="Logo Autoservicio Popular" />
+            <img onClick={() => handleClick('/')} loading='lazy' id="logo" src={logo} alt="Logo Autoservicio Popular" />
         </div>
         <div id="center" class="TopBarElements">
             <input id="SearchInput" type="text" placeholder="Buscar Producto"/>
-            <button id="SearchButton">
+            <button onClick={() => handleClick('/buscador')} id="SearchButton">
                 <img loading='lazy' id="SearchImg" src={lupa} alt="Lupa color negro" />
             </button>
         </div>
         <div id="left" class="TopBarElements">
-            <img loading='lazy' src={carrito} class="leftImg" alt="Icono carrito de compras"/>
-            <img loading='lazy' src={user} class="leftImg" alt="Icono de usuario"/>
+            <img onClick={() => handleClick('/carrito')} loading='lazy' src={carrito} class="leftImg" alt="Icono carrito de compras"/>
+            <img onClick={() => handleClick('/login')} loading='lazy' src={user} class="leftImg" alt="Icono de usuario"/>
         </div>
     </nav>
   );
